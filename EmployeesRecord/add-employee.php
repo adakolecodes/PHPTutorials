2a6ac2e4c7fbd8a76815ec306de466904af9b521
adakolecodes <amdeezy.com@gmail.com>
2024-05-31 13:11:12 +0100

    added a new app

---
+++ EmployeesRecord/add-employee.php
@@ -0,0 +1,78 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Employee</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container mt-5 mb-5">
+        <h1 class="mb-5">Add Employee</h1>
+        <?php
+            if(isset($_SESSION['error'])){
+                echo '<div class="alert alert-danger" role="alert">'.$_SESSION['error'].'</div>';
+                unset($_SESSION['error']);
+            }
+
+            if(isset($_SESSION['success'])){
+                echo '<div class="alert alert-success" role="alert">'.$_SESSION['success'].'</div>';
+                unset($_SESSION['success']);
+            }
+        ?>
+        <form action="processes/add-employee-process.php" method="post">
+            <div class="row mb-3">
+                <div class="col-md-6">
+                    <input type="text" class="form-control" name="firstName" placeholder="First Name" required>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-md-6">
+                    <input type="text" class="form-control" name="lastName" placeholder="Last Name" required>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-md-6">
+                    <select class="form-select" name="gender" required>
+                        <option selected disabled value="">Select gender</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-md-6">
+                    <input type="number" class="form-control" name="age" placeholder="Age" required>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-md-6">
+                    <input type="text" class="form-control" name="occupation" placeholder="Occupation" required>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-md-6">
+                    <input type="number" class="form-control" name="income" placeholder="Income" required>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-md-6">
+                    <select class="form-select" name="married" required>
+                        <option selected disabled value="">Select marital status</option>
+                        <option value="true">Married</option>
+                        <option value="false">Single</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-md-6">
+                    <button type="submit" name="submit" class="btn btn-dark">Add Employee</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ EmployeesRecord/processes/add-employee-process.php
@@ -0,0 +1,40 @@
+<?php
+
+if(isset($_POST['submit'])){
+    session_start();
+
+    include_once "../config/db-connect.php";
+
+    $POST = $POST = filter_var_array($_POST, FILTER_UNSAFE_RAW);
+    $firstName = $POST['firstName'];
+    $lastName = $POST['lastName'];
+    $gender = $POST['gender'];
+    $age = $POST['age'];
+    $occupation = $POST['occupation'];
+    $income = $POST['income'];
+    $married = $POST['married'];
+
+    //Prevent submission of empty inputs
+    if(empty($firstName) || empty($lastName) || empty($gender) || empty($age) || empty($occupation) || empty($income) || empty($married)){
+        $_SESSION['error'] = "Please fill in all fields";
+        header("Location: ../add-employee.php");
+        exit();
+    }
+
+    //Insert data into database
+    $sql = "INSERT INTO employees (firstName, lastName, gender, age, occupation, income, married) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$firstName, $lastName, $gender, $age, $occupation, $income, $married]);
+
+    //Show success or error message if inserted successfully or failed
+    if($stmt->rowCount() > 0){
+        $_SESSION['success'] = "Employee added successfully";
+        header("Location: ../add-employee.php");
+    }else{
+        $_SESSION['error'] = "Failed to add employee";
+        header("Location: ../add-employee.php");
+    }
+
+}
+
+?>
